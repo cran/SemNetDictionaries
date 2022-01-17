648e912ee7d2837ab73a10cdb37c66e45cb5ab71
Alexander P. Christensen <alexpaulchristensen@gmail.com>
2022-01-17 13:12:43 +0000

    version 0.2.0

---
+++ R/cocaspell.moniker.R
@@ -0,0 +1,22 @@
+#COCA and Hunspell Moniker----
+#' Corpus of Contemporary American English and Hunspell Moniker
+#'
+#' A database of word forms for the Corpus of Contemporary American English and Hunspell dictionaries
+#' 
+#' To add additional COCA and Hunspell monikers to the database, please submit a pull
+#' request or issue to \href{https://github.com/AlexChristensen/SemNetDictionaries}{https://github.com/AlexChristensen/SemNetDictionaries}
+#' 
+#' @name cocaspell.moniker
+#' 
+#' @docType data
+#' 
+#' @usage data(cocaspell.moniker)
+#' 
+#' @format cocaspell.moniker (list, length = 29610)
+#' 
+#' @keywords datasets
+#' 
+#' @examples 
+#' data("cocaspell.moniker")
+NULL
+#----
---
+++ R/most_common.dictionary.R
@@ -0,0 +1,23 @@
+#Most Common Dictionary----
+#' Most Common Dictionary
+#'
+#' A general dictionary of 10,000 of the most common U.S. English words derived from
+#' \href{https://github.com/first20hours/google-10000-english}{https://github.com/first20hours/google-10000-english}.
+#' 
+#' To add additional words to the dictionary, please make an
+#' appendix dictionary (\code{\link[SemNetDictionaries]{append.dictionary}})
+#' 
+#' @name most_common.dictionary
+#' 
+#' @docType data
+#' 
+#' @usage data(most_common.dictionary)
+#' 
+#' @format most_common.dictionary (vector, length = 9329)
+#' 
+#' @keywords datasets
+#' 
+#' @examples 
+#' data("most_common.dictionary")
+NULL
+#----
